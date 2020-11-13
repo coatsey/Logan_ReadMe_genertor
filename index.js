b669ffc02e37e07303dfd137fe26b977cb59de36
Logan Coates <coates.logan1125@gmail.com>
2020-11-13 15:07:54 -0700

    started the index.js

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-// array of questions for user
-const questions = [
-
-];
-
-// function to write README file
-function writeToFile(fileName, data) {
-}
-
-// function to initialize program
-function init() {
-
-}
-
-// function call to initialize program
-init();
+const inquirer = require("inquirer");
+const fs = require('fs');
+const axios = require('axios');
