a966e40803045d9ad25ef820900d886ea1fa4b5e
Logan Coates <coates.logan1125@gmail.com>
2020-11-13 15:42:20 -0700

    finished java file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require("inquirer");
 const fs = require('fs');
 const axios = require('axios');
+
 inquirer.prompt([
     {
         type: "input",
@@ -27,7 +28,7 @@ inquirer.prompt([
         name: "usage",
         message: "Please explain the usage for your project:"
     }
-]).then(function(response) {
+  ]).then(function(response) {
     let {name,gitname,title,description,usage} = response;
     let avatarUrl;
     let email;
@@ -44,3 +45,38 @@ inquirer.prompt([
             };
 
             let filename = name.toLowerCase().split(' ').join('_') + ".md";
+           
+let data = `# ${title}
+Author: ${name}
+## Description
+${description}
+## Table of Contents 
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Tests](#tests)
+* [Questions](#questions)
+## Installation
+To install necessary dependencies, run the following command:
+npm install
+## Usage
+${usage}
+## License
+This project is licensed under the MIT license.
+## Tests
+To run tests, run the following command:
+npm test
+## Questions
+[![GitHub license](https://img.shields.io/badge/GitHubUser-${gitname}-orange)](${queryUrl})
+If you have any questions about the repo, open an issue or contact me directly [here](${queryUrl})${email}`;
+
+    fs.writeFile(filename, data, function(error) {
+        if(error) {
+            return console.log(error);
+        } 
+        console.log("Your README file has been generated.");
+    })
+        }
+    );
+    
+  });
